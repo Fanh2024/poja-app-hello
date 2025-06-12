fcaa34b9984adfe6cdcebebb958af32690b72c18
Fanh2024 <hei.fanhasina@gmail.com>
2025-06-12 11:39:38 +0300

    feat: /hello with S3 file upload and presigned url

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,13 +1,28 @@
 package com.my.company.endpoint.rest.controller.health;
 
+import com.my.company.service.HelloWorldService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    /*
     @GetMapping("/hello")
     public String helloWorld() {
         return "... world!";
+    }
+     */
+
+    private final HelloWorldService service;
+
+    public HelloWorldController(HelloWorldService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(@RequestParam String name) {
+        return service.uploadHelloWorldMessage(name);
     }
 }
